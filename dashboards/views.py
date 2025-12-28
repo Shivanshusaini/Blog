f9009861061e304dbb7d45236191abf878d982bd
Shivanshu Saini <shivanshusaini707@gmail.com>
2025-12-28 18:32:52 +0530

    login decorator

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from blogs.models import Category,Blogs
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='login')
 def dashboards(request):
     category_count = Category.objects.all().count()
     blogs_count = Blogs.objects.all().count()
